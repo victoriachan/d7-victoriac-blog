17996bf034c47c785dcfc2e453ac749a455e3e28
Victoria Chan <contact@victoriac.net>
2012-04-09 21:39:35 +0100

    Added custom theme from mothership example

---
+++ themes/fly/template.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Preprocess
+*/
+
+/*
+function fly_preprocess_html(&$vars) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_page(&$vars,$hook) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_region(&$vars,$hook) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_block(&$vars, $hook) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_node(&$vars,$hook) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_comment(&$vars,$hook) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_field(&$vars,$hook) {
+  //  kpr($vars['content']);
+}
+
+function fly_preprocess_maintenance_page(){
+  //  kpr($vars['content']);
+}
+*/
